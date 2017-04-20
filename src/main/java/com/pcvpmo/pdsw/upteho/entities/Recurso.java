fdeae766782ea8f0aa9ae5cb2678561a892f4fc9
Juan Camilo Mantilla <juan.mantilla-r@mail.escuelaing.edu.co>
2017-04-20 12:16:45 -0500

    - Adicion de las clases en Entities (vacias) 	- Eliminado archivo anzuelo en el paquete entities

---
+++ src/main/java/com/pcvpmo/pdsw/upteho/entities/Recurso.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pcvpmo.pdsw.upteho.entities;
+
+/**
+ *
+ * @author 2116177
+ */
+public class Recurso {
+    
+}
